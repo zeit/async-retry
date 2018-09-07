f7b4a09ab18f44e9f54ec4817522e4d44a69d2e4
Leo Lamprecht <mindrun@icloud.com>
2018-09-07 10:43:30 +0200

    Made it work on old browsers

--- examples/wikipedia.js
+++ examples/wikipedia.js
@@ -1,15 +1,16 @@
-import fetch from 'node-fetch'
-// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
-import cheerio from 'cheerio'
-import retry from '../' // eslint-disable-line import/no-unresolved
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved */
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+import retry from '..';
+/* eslint-enable import/no-extraneous-dependencies, import/no-unresolved */
 
-const getRandomTitle = async () => {
-  return await retry(async () => {
-    const res = await fetch('https://en.wikipedia.org/wiki/Special:Random')
-    const text = await res.text()
-    const $ = cheerio.load(text)
-    return $('h1').text()
-  })
-}
+const getRandomTitle = async () =>
+  retry(async () => {
+    const res = await fetch('https://en.wikipedia.org/wiki/Special:Random');
+    const text = await res.text();
+    const $ = cheerio.load(text);
+    return $('h1').text();
+  });
 
-getRandomTitle().then(console.log)
+// eslint-disable-next-line no-console
+getRandomTitle().then(console.log);
